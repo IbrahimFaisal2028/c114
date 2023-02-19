710bf5e5ac9dd162b615933c40df921f7236b3b3
IbrahimFaisal2028 <108027026+IbrahimFaisal2028@users.noreply.github.com>
2023-02-18 19:18:51 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+nosex = 0;
+nosey = 0;
 function preload()
 {
 }
@@ -5,12 +7,30 @@ function setup()
 {
 canvas = createCanvas(300, 300);
 canvas.center();
+video = createCapture(VIDEO);
+video.hide;
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses);
 }
 function draw()
 {
-
+image(video, 0, 0, 300, 300);
 }
 function takeSnapshot()
 {
     save('filterimage.png');
+}
+function modelLoaded()
+{
+    console.log("posenet module is initialized");
+}
+function gotPoses(results)
+{
+if(results.length > 0)
+{
+    console.log(results);
+    nosex = results[0].pose.nose.x;
+    nosey = results[0].pose.nose.y;
+    console.log("nosex = " + nosex + " | nosey = " + nosey);
+}
 }
